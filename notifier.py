43d6f47a4569be6284b5d82a62e84cbf77c6ff0f
Ole Eskild Steensen <oleeskilds@gmail.com>
2020-09-11 21:20:02 +0000

    Added functionality for logging in with access_token, to prevent login emails from tgtg

--- notifier.py
+++ notifier.py
@@ -6,10 +6,16 @@ from twilio.rest import Client
 
 email = os.getenv("TGTG_EMAIL")
 password = os.getenv("TGTG_PASSWORD")
+access_token = os.getenv("TGTG_ACCESS_TOKEN")
+user_id = os.getenv("TGTG_USER_ID")
 account_sid = os.getenv("TWI_ACCOUNT_SID")
 auth_token = os.getenv("TWI_AUTH")
 
-client = TgtgClient(email=email, password=password)
+if(access_token == None or user_id ==None):
+    client = TgtgClient(email=email, password=password)
+else:
+    client = TgtgClient(access_token=access_token, user_id=user_id)
+
 sms_client = Client(account_sid, auth_token)
 
 favs = client.get_items()
@@ -19,8 +25,8 @@ if number_available == 1:
 else:
     to_number = os.getenv("TO_NUMBER")
     sms_client.messages.create(
-                body=('Antall fra BB Sletten: ' + number_available),
-                from_='TgTg Elon',
+                body=('Antall fra BB Sletten: ' + str(number_available)),
+                from_='Elon Musk',
                 to=to_number
             )
 
